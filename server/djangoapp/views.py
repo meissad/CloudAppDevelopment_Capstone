2472749a2ccbdfd8c96901708caccf1d3c6fb744
meissad <sameiidia1@gmail.com>
2024-02-01 08:41:23 -0500

    Static Pages

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
@@ -18,24 +19,61 @@ logger = logging.getLogger(__name__)
 
 
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
+def about(request):
+    return render(request, 'djangoapp/about.html')
+
 
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
+def contact(request):
+    return render(request, 'djangoapp/contact.html')
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("djangoapp:index"))
+        else:
+            return render(request, "djangoapp/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "djangoapp/login.html")
+
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("djangoapp:index"))
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+    if request.method == "POST":
+        first_name = request.POST["firstname"]
+        last_name = request.POST["lastname"]
+        user_name = request.POST["username"]
+        password = request.POST["password"]
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username=user_name, first_name=first_name, last_name=last_name,password=password)
+        except IntegrityError as e:
+            print(e)
+            return render(request, "djangoapp/registration.html", {
+                "message": "username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("djangoapp:index"))
+    else:
+        return render(request, "djangoapp/registration.html")
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
