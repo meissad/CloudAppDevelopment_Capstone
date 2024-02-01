8d3a06077663ee5c2d2375ee00528877ac821757
meissad <sameiidia1@gmail.com>
2024-02-01 11:30:32 -0500

    Models done

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
 # from .models import related models
-
+from .models import CarMake, CarModel
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 3
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['Name']
+
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ['Name']
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,14 +4,41 @@ from django.utils.timezone import now
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
+# <HINT> Create a Car Make model 
+class CarMake(models.Model):
+    Name = models.CharField(null=False, max_length=30)
+    Description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "Name: " + self.Name + "," + \
+            "Description: " + self.Description
 # - Name
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
+# <HINT> Create a Car Model model 
+class CarModel(models.Model):
+
+    Types = [
+        ('sedan', 'SEDAN'),
+        ('suv', 'SUV'),
+        ('wagon', 'WAGON')
+    ]
+
+    Car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    Name = models.CharField(null=False, max_length=30)
+    Dealer_id = models.IntegerField()
+    Type = models.CharField(null=False, max_length=30, choices=Types)
+    Year = models.DateField()
+
+    def __str__(self):
+        return  "Car Make: " + self.Car_make + ", " + \
+                "Name: " + self.Name + ", " \
+                "Year: " + str(self.Year) + ", " + \
+                "Dealer ID: " + self.Dealer_id + ", " + \
+                "Type: " + self.Type
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
 # - Dealer id, used to refer a dealer created in cloudant database
